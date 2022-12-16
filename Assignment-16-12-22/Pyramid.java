ac55518b667dbc5fd267043eeac32a0b9fd61d44
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2022-12-16 19:06:01 +0530

    Create Pyramid.java

---
+++ Assignment-16-12-22/Pyramid.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		int n=5;
+		for (int i=5;i>0;i--) {
+			for (int j=0;j<i;j++) {
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+		}
+		
+		
+		
+			
+
+	}
+
+}
