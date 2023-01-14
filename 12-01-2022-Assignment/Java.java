bc5d450cbe14c12ea82d27a4f4ae5334d1f86c23
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-01-14 11:14:01 +0530

    Create Java.java

---
+++ 12-01-2022-Assignment/Java.java
@@ -0,0 +1,49 @@
+package batchapplication;
+
+public class Java {
+
+	private int Studentid;
+	private String stdName;
+	private long phone;
+	private String email;
+	public int getStudentid() {
+		return Studentid;
+	}
+	public void setStudentid(int studentid) {
+		Studentid = studentid;
+	}
+	public String getStdName() {
+		return stdName;
+	}
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Java(int studentid, String stdName, long phone, String email) {
+		super();
+		Studentid = studentid;
+		this.stdName = stdName;
+		this.phone = phone;
+		this.email = email;
+	}
+	public Java() {
+		super();
+		
+	}
+	
+	
+	
+	
+	
+}
