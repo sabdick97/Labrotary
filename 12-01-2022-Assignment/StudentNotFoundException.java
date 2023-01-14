327d5c501b14dc089933cd4bb4f50fa7ede7f152
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-01-14 11:35:02 +0530

    Create StudentNotFoundException.java

---
+++ 12-01-2022-Assignment/StudentNotFoundException.java
@@ -0,0 +1,13 @@
+package batchapplication;
+
+public class StudentNotFoundException extends RuntimeException {
+	
+	
+	
+
+	public StudentNotFoundException(String message) {
+		super(message);
+		
+	}
+
+}
