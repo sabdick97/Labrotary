fc675c8c4e058c97d951ef197af127884cb2feb9
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-01-23 22:01:20 +0530

    Create Character.java

---
+++ Practice Basic Programme/Character.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Character {
+
+	public static void main(String[] args) {
+		char ch;
+		System.out.println("Enter any character");
+		Scanner sc=new Scanner(System.in);
+		ch=sc.next().charAt(0);
+		System.out.println(ch);
+	}
+	
+
+}
