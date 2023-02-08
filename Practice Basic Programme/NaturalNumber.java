0a58e0b18d01fadbda45f85937200b19fc98a0e7
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-02-08 22:12:10 +0530

    Create NaturalNumber.java

---
+++ Practice Basic Programme/NaturalNumber.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class NaturalNumber {
+
+	public static void main(String[] args) {
+		int a; 
+		System.out.println("enter any number");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		for(a=1;a<=10;a++)
+		{ 	
+			System.out.println(a+ " ");
+		}
+	}
+}
+	
