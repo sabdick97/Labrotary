422cef93967c7ff7de07300d0392e70663a73dec
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-02-09 15:27:50 +0530

    Create ReverseArray.java

---
+++ ThursdayLab(09-02-23)/ReverseArray.java
@@ -0,0 +1,29 @@
+package thrsdayJob;
+//WAP to reverse an array
+import java.util.Scanner;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		int a[]=new int[5];
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Enter Array Elements");
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Printing Array Elements");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		
+		System.out.println("\nReverse the Array Elements");
+		
+		for(int i=a.length-1;i>=0;i--)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
