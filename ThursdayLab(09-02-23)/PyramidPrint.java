6fc5541ca7ce735588a934723e213950687be5c4
sabdick97 <120620999+sabdick97@users.noreply.github.com>
2023-02-09 15:28:26 +0530

    Create  PyramidPrint.java

---
+++ ThursdayLab(09-02-23)/PyramidPrint.java
@@ -0,0 +1,19 @@
+package thrsdayJob;
+//WAP to print a pyramid
+public class PyramidPrint {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				
+				System.out.print('*');
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
